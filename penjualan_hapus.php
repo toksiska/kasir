2bccd5f1666d7a220dd5dba8e3b20f548acfaa8b
toksiska <150230467+toksiska@users.noreply.github.com>
2025-01-15 15:37:57 +0700

    Add files via upload

---
+++ penjualan_hapus.php
@@ -0,0 +1,9 @@
+<?php
+$id = $_GET['id'];
+$query = mysqli_query($koneksi, "DELETE FROM penjualan WHERE id_penjualan=$id");
+$query = mysqli_query($koneksi, "DELETE FROM detail_penjualan WHERE id_penjualan=$id");
+if($query) {
+    echo '<script>alert("Hapus data berhasil"); location.href="?page=penjualan"</script>';
+}else{
+    echo '<script>alert("Hapus data gagal")</script>';
+}
